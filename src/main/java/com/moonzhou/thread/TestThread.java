24afbd1be3da2278cb2254608b1428762b9fbff9
moon-zhou <ayimin1989@163.com>
2017-06-21 23:27:06 +0800

    thread demo
    extend thread

--- src/main/java/com/moonzhou/thread/TestThread.java
+++ src/main/java/com/moonzhou/thread/TestThread.java
@@ -3,10 +3,19 @@ package com.moonzhou.thread;
 public class TestThread {
 
     public static void main(String args[]) {
-        RunnableDemo R1 = new RunnableDemo("Thread-1");
+        /*RunnableDemo R1 = new RunnableDemo("Thread-1");
         R1.start();
 
         RunnableDemo R2 = new RunnableDemo("Thread-2");
-        R2.start();
+        R2.start();*/
+
+        System.out.println("===========================");
+
+        ThreadDemo T1 = new ThreadDemo("Thread-1");
+        T1.start();
+
+        ThreadDemo T2 = new ThreadDemo("Thread-2");
+        T2.start();
+
     }
 }
