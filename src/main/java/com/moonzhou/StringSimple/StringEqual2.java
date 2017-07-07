6d749e8d352f2a639be0787b374880c4940b1bc6
moon-zhou <ayimin1989@163.com>
2017-07-08 00:06:44 +0800

    String intert

--- src/main/java/com/moonzhou/StringSimple/StringEqual2.java
+++ src/main/java/com/moonzhou/StringSimple/StringEqual2.java
@@ -7,8 +7,8 @@ public class StringEqual2 {
 
     @Test
     public void equalTrue() {
-        String str0 = new String("123"); // 堆中生辰对象b， 字符串池中创建字符串“123"
-        String str1 = "123";// 字符串池中有“123"字符串，不再创建
+        String str0 = new String("123"); // 堆中生辰对象b， 字符串池中创建字符串"123"
+        String str1 = "123";// 字符串池中有"123"字符串，不再创建
 
         Assert.assertEquals(str0.hashCode(), str1.hashCode());
         Assert.assertFalse(str0 == str1);
@@ -38,5 +38,22 @@ public class StringEqual2 {
         Assert.assertFalse(s0 == s2);
 
         Assert.assertFalse(s1 == s2);
+    }
+
+    @Test
+    public void intert() {
+        String s0 = "kvill";
+        String s1 = new String("kvill");
+        String s2 = new String("kvill");
+
+        Assert.assertFalse(s0 == s1);
+
+        s1.intern();
+        s2 = s2.intern(); // 把常量池中"kvill"的引用赋给s2
+
+        Assert.assertFalse(s0 == s1);
+
+        Assert.assertTrue(s0 == s1.intern());
+        Assert.assertTrue(s0 == s2);
     }
 }
