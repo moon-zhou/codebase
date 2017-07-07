57f5961f0a82bbac0804f84f8f30dac3897326bf
moon-zhou <ayimin1989@163.com>
2017-07-07 23:56:06 +0800

    string equals
    add comments

--- src/main/java/com/moonzhou/StringSimple/StringEqual2.java
+++ src/main/java/com/moonzhou/StringSimple/StringEqual2.java
@@ -4,11 +4,11 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class StringEqual2 {
-    
+
     @Test
     public void equalTrue() {
-        String str0 = new String("123");
-        String str1 = "123";
+        String str0 = new String("123");    // 堆中生辰对象b， 字符串池中创建字符串“123”
+        String str1 = "123";// 字符串池中有“123”字符串，不再创建
 
         Assert.assertEquals(str0.hashCode(), str1.hashCode());
         Assert.assertFalse(str0 == str1);
