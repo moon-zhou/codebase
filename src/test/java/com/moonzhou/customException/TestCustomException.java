0bf31184aa7d45f7d34c0d2e666c6e55dacd270c
moon-zhou <ayimin1989@163.com>
2017-07-27 21:49:12 +0800

    custome exception
    runtime exception

--- src/test/java/com/moonzhou/customException/TestCustomException.java
+++ src/test/java/com/moonzhou/customException/TestCustomException.java
@@ -9,7 +9,7 @@ public class TestCustomException {
 
     @Test
     public void testDivition() {
-        
+
         Division num = new Division();
 
         try {
@@ -20,6 +20,18 @@ public class TestCustomException {
         } catch (Exception e) {
             System.out.println(e.getMessage());
             e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void testMyException() {
+        String[] sexs = { "男性", "女性", "中性" };
+        for (int i = 0; i < sexs.length; i++) {
+            if ("中性".equals(sexs[i])) {
+                throw new MyException("你全家都是中性！");// 此处在junit里打印
+            } else {
+                System.out.println(sexs[i]);
+            }
         }
     }
 }
