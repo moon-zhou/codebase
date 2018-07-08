c76a3e16165d146922a65094da7e248217685723
moon-zhou <ayimin1989@163.com>
2018-07-08 16:36:52 +0800

    demo地址

--- src/main/java/com/moonzhou/thread/countDownLatchDemo/checker/Test.java
+++ src/main/java/com/moonzhou/thread/countDownLatchDemo/checker/Test.java
@@ -1,4 +1,15 @@
 package com.moonzhou.thread.countDownLatchDemo.checker;
+
+/**
+ * 
+ * Desc: <funtion desc> <br>
+ * Detail: http://www.importnew.com/15731.html. <br>
+ * Date: 2018年7月8日 下午4:31:33 <br>
+ * 
+ * @author moon-zhou
+ * @version
+ * @since 2018年7月8日
+ */
 public class Test {
     public static void main(String[] args)
     {
